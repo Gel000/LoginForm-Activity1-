23a0d11a6306f3e393b56289eff97fdfdc8fb6f7
Gel000 <146512237+Gel000@users.noreply.github.com>
2024-02-10 00:28:14 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Loginfrm } from './Components/Login_Form/Loginfrm';
+
+function App() {
+  return (
+    <div>
+      <Loginfrm />
+    </div>
+  );
+}
+
+export default App;
